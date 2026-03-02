735ce1c9d54d09069b6cbc48b0e5f919bd33a86a
straintrack8-afk <straintrack8@gmail.com>
2026-03-02 15:38:07 +0700

    fix: update base path for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcssPlugin from '@tailwindcss/postcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FSPigFarm/',
+  base: '/',
   css: {
     postcss: {
       plugins: [
